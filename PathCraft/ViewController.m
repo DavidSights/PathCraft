888b9820b768a050e39d44a8bc9a0d1d51d2a267
Michael Johnson <msjohnso2003@gmail.com>
2015-07-11 14:57:43 -0500

    commit.

--- PathCraft/ViewController.m
+++ PathCraft/ViewController.m
@@ -141,33 +141,4 @@
     }
 }
 
-# pragma mark - Utilty
-+(BOOL)isRollSuccessfulWithNumberOfDice:(NSUInteger)numberOfDice
-                                  sides:(NSUInteger)sides
-                                  bonus:(NSInteger)bonus
-                          againstTarget:(NSInteger)target {
-    NSInteger value = [self rollValueWithNumberOfDice:numberOfDice sides:sides bonus:bonus];
-    if(value >= target) {
-        return YES;
-    }
-    return NO;
-}
-
-+(NSInteger)rollValueWithNumberOfDice:(NSUInteger)numberOfDice sides:(NSUInteger)sides bonus:(NSInteger)bonus {
-    NSInteger total = 0;
-    for (int i=0; i<numberOfDice; i++) {
-        NSUInteger roll = [self rollDieWithSides:sides];
-        total+=roll;
-    }
-    return total+bonus;
-}
-
-+ (NSUInteger)rollDieWithSides:(NSUInteger)sides {
-    // I'm adding this assertion here because I am assuming our dice have a small number of sides so that
-    // casting from a 64-bit NSUInteger to a 32 bit unsigned int for the arc4random_uniform roll
-    // isn't a problem. If our dice more have more than 10000 sides at any point, we should revisit this assumption.
-    NSAssert(sides<10000, @"Die has too many sides.");
-    return arc4random_uniform((u_int32_t)sides)+1;
-}
-
 @end
