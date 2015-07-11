1b4180abc18e4b981d26687e2936d711b68cca5a
Pablq <pabloignaciophilipps@gmail.com>
2015-07-11 15:16:23 -0500

    initial mountain environment implemented.

--- PathCraft/Mountain.m
+++ PathCraft/Mountain.m
@@ -43,7 +43,7 @@
     
     // Cliff
     NSString *cliff1 = @"Move forward for a closer look."; // R: 1) You slip and fall to your death. 2) You are rewarded with an incredible breeze and view. You can see (other environment) from here. 3) You are overcome with fear... You force yourself to crawl for a peak.
-    NSString *cliff2 = @"Take a leap of faith."; // R: 1) You fall to your death. 2) A mysterious wind catches you and places you safely on solid ground.
+    NSString *cliff2 = @"Take a leap of faith over the edge."; // R: 1) You fall to your death. 2) A mysterious wind catches you and places you safely on solid ground.
 
     // Peak
     NSString *peak1 = @"Scurry to the top."; // R: 1) You scream at the top of your lungs. This is the most free you've ever felt. 2) You are left speechless. What can be said about such a sensation.
@@ -56,11 +56,6 @@
     
     
     /* Regular */
-    
-    //Event *event1 = [Event new];
-    //event1.eventDescription = @"Trees surround you all around.";
-    //NSArray *choices = [NSArray arrayWithObjects:moveForward, nil];
-    //event1.choices = choices;
     
     Event *event1 = [Event new];
     event1.eventDescription = @"The air is crisp here.";
@@ -87,33 +82,75 @@
     choices = [NSArray arrayWithObjects:moveForward, moveBackward, nil];
     event5.choices = choices;
     
-    // Sublime feeling of awe
+    Event *event6 = [Event new];
+    event6.eventDescription = @"The trail is dangerous. Watch your footing.";
+    choices = [NSArray arrayWithObjects:moveForward, moveBackward, nil];
+    event6.choices = choices;
     
-    // The trail is difficult (watch your footing).
-    
-    // A false summit.
+    Event *event7 = [Event new];
+    event7.eventDescription = @"It's a false summit.";
+    choices = [NSArray arrayWithObjects:moveForward, moveBackward, nil];
+    event7.choices = choices;
     
     /* Unique */
     
-    // Cave
+    Event *uniqueEvent1 = [Event new];
+    uniqueEvent1.eventDescription = @"You encounter a cave. It looks warm and inviting.";
+    choices = [NSArray arrayWithObjects: cave1, moveForward, moveBackward, nil];
+    uniqueEvent1.choices = choices;
+    uniqueEvent1.isUnique = YES;
     
-    // Procession
+    Event *uniqueEvent2 = [Event new];
+    uniqueEvent2.eventDescription = @"You come across a procession of locals in bright hats.";
+    choices = [NSArray arrayWithObjects: procession1, procession2, moveForward, moveBackward, nil];
+    uniqueEvent2.choices = choices;
+    uniqueEvent2.isUnique = YES;
     
-    // Cliff
+    Event *uniqueEvent3 = [Event new];
+    uniqueEvent3.eventDescription = @"Suddenly you find yourself at a cliff. You cannot see what's beyond it.";
+    choices = [NSArray arrayWithObjects: cliff1, cliff2, moveBackward, nil];
+    uniqueEvent3.choices = choices;
+    uniqueEvent3.isUnique = YES;
     
-    // Peak
+    Event *uniqueEvent4 = [Event new];
+    uniqueEvent4.eventDescription = @"Suddenly you find yourself at a cliff. You cannot see what's beyond it.";
+    choices = [NSArray arrayWithObjects: cliff1, cliff2, moveBackward, nil];
+    uniqueEvent4.choices = choices;
+    uniqueEvent4.isUnique = YES;
     
-    // Demi-god presents itself
+    Event *uniqueEvent5 = [Event new];
+    uniqueEvent5.eventDescription = @"You've arrived at the summit!";
+    choices = [NSArray arrayWithObjects: peak1, peak2, moveBackward, nil];
+    uniqueEvent5.choices = choices;
+    uniqueEvent5.isUnique = YES;
+    
+    Event *uniqueEvent6 = [Event new];
+    uniqueEvent6.eventDescription = @"A mystical being appears nearby.";
+    choices = [NSArray arrayWithObjects: demiGod1, demiGod2, demiGod3, moveForward, moveBackward, nil];
+    uniqueEvent6.choices = choices;
+    uniqueEvent6.isUnique = YES;
     
     /* Combat */
     
-    // Lion
+    Event *combatEvent1 = [Event new];
+    combatEvent1.eventDescription = @"You encounter a mountain lion.";
+    choices = [NSArray arrayWithObjects: fight, flee, nil];
+    combatEvent1.choices = choices;
     
-    // Bandits
+    Event *combatEvent2 = [Event new];
+    combatEvent2.eventDescription = @"You encounter a group of bandits.";
+    choices = [NSArray arrayWithObjects: fight, flee, nil];
+    combatEvent2.choices = choices;
     
-    // Angry bird
+    Event *combatEvent3 = [Event new];
+    combatEvent3.eventDescription = @"An eagle swoops down and attacks you.";
+    choices = [NSArray arrayWithObjects: fight, flee, nil];
+    combatEvent3.choices = choices;
     
-    // Demi-god presents itself
+    Event *combatEvent4 = [Event new];
+    combatEvent4.eventDescription = @"A mystical being comes towards you angrily.";
+    choices = [NSArray arrayWithObjects: fight, flee, nil];
+    combatEvent4.choices = choices;
     
     /* Craftables */
     
@@ -125,105 +162,9 @@
     
     // Demi-god presents magical material to you
     
-    NSArray *events = [NSArray arrayWithObjects: event1, nil];
+    NSArray *events = [NSArray arrayWithObjects: event1, event2, event4, event4, event5, event6, event7, uniqueEvent1, uniqueEvent2, uniqueEvent3, uniqueEvent4, uniqueEvent5, uniqueEvent6, combatEvent1, combatEvent2, combatEvent3, combatEvent4, nil];
     
     return events;
 }
-
-//
-//
-//// Passive Events
-//
-//Event *event1 = [Event new];
-//event1.eventDescription = @"Trees surround you all around.";
-//NSArray *choices = [NSArray arrayWithObjects:moveForward, nil];
-//event1.choices = choices;
-//
-//Event *event2 = [Event new];
-//event2.eventDescription = @"Sun light peaks through leaves of a tree.";
-//choices = [NSArray arrayWithObjects:moveForward, moveBackward, nil];
-//event2.choices = choices;
-//
-//Event *event3 = [Event new];
-//event3.eventDescription = @"You think you hear something. You wait and nothing happens.";
-//choices = [NSArray arrayWithObjects:moveForward, moveBackward, nil];
-//event3.choices = choices;
-//
-//Event *event4 = [Event new];
-//event4.eventDescription = @"Your feet are kind of sore.";
-//choices = [NSArray arrayWithObjects:moveForward, moveBackward, nil];
-//event4.choices = choices;
-//
-//Event *event5 = [Event new];
-//event5.eventDescription = @"The smell of nature is thick here.";
-//choices = [NSArray arrayWithObjects:moveForward, moveBackward, nil];
-//event5.choices = choices;
-//
-//Event *event6 = [Event new];
-//event6.eventDescription = @"You notice you smell kind of bad. Oh well.";
-//choices = [NSArray arrayWithObjects:moveForward, moveBackward, nil];
-//event6.choices = choices;
-//
-//Event *event7 = [Event new];
-//event7.eventDescription = @"A bird flies past your face. A little too close for comfort.";
-//choices = [NSArray arrayWithObjects:moveForward, moveBackward, nil];
-//event7.choices = choices;
-//
-//// Unique Events
-//
-//Event *uniqueEvent1 = [Event new];
-//uniqueEvent1.eventDescription = @"Your foot gets caught in a root trap.";
-//choices = [NSArray arrayWithObjects:root1, root2, nil];
-//uniqueEvent1.choices = choices;
-//uniqueEvent1.isUnique = YES;
-//
-//Event *uniqueEvent2 = [Event new];
-//uniqueEvent2.eventDescription = @"You hear banging like sticks against a tree.";
-//choices = [NSArray arrayWithObjects: banging1, moveForward, moveBackward, nil];
-//uniqueEvent2.choices = choices;
-//uniqueEvent2.isUnique = YES;
-//
-//Event *uniqueEvent3 = [Event new];
-//uniqueEvent3.eventDescription = @"You find a fruit tree. Fallen fruits litter the ground.";
-//choices = [NSArray arrayWithObjects: tree1, tree2, moveForward, moveBackward, nil];
-//uniqueEvent3.choices = choices;
-//uniqueEvent3.isUnique = YES;
-//
-//Event *uniqueEvent4 = [Event new];
-//uniqueEvent4.eventDescription = @"You notice glowing eyes in a thick bush.";
-//choices = [NSArray arrayWithObjects: eyes1, moveForward, moveBackward, nil];
-//uniqueEvent4.choices = choices;
-//uniqueEvent4.isUnique = YES;
-//
-//// Combat Events --- Stretch goal: Strength Levels!
-//
-//Event *combatEvent1 = [Event new];
-//combatEvent1.eventDescription = @"You encounter a giant spider.";
-//choices = [NSArray arrayWithObjects: fight, flee, nil];
-//combatEvent1.choices = choices;
-//
-//Event *combatEvent2 = [Event new];
-//combatEvent2.eventDescription = @"A wild and hungry snake appears.";
-//choices = [NSArray arrayWithObjects: fight, flee, nil];
-//combatEvent2.choices = choices;
-//
-//Event *combatEvent3 = [Event new];
-//combatEvent3.eventDescription = @"A giant, rabid wolf appears.";
-//choices = [NSArray arrayWithObjects: fight, flee, nil];
-//combatEvent3.choices = choices;
-//
-//// Result Event
-//
-//
-///*
-// Event *event<#number#> = [Event new];
-// event1.eventDescription = @"<#Description#>";
-// choices = [NSArray arrayWithObjects:moveForward, moveBackward, nil];
-// event<#number#>.choices = choices;
-// */
-//
-//self.events = [NSArray arrayWithObjects: event1, event2, event3, event4, event5, event6, event7,
-//               uniqueEvent1, uniqueEvent2, uniqueEvent3, uniqueEvent4,
-//               combatEvent1, combatEvent2, combatEvent3, nil];
 
 @end
