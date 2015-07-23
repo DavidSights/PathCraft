254c57a844def0c97a4500d650fb2c0b48cf9a85
Pablq <pabloignaciophilipps@gmail.com>
2015-07-22 21:26:01 -0500

    implemented moveBackward method.

--- PathCraft/Game.m
+++ PathCraft/Game.m
@@ -18,8 +18,8 @@
     Environment *currentEnvironment;
     NSString *currentEventDescription;
     Player *player;
-    NSMutableArray *fullEventHistory;
-    NSMutableArray *eligibleEventHistory; // must always have at least one event!
+    NSMutableArray *fullEventHistory; // always has at least one event (starting with getInitialEvent). all events get added.
+    NSMutableArray *eventHistory; // must always have at least one event (starting with getInitialEvent). combat and unique events do not get added!
     Dice *dice;
     NSInteger score;
     NSDictionary *selectorsForChoiceDescription;
@@ -37,8 +37,8 @@
         
         player = [Player new];
         
+        eventHistory = [NSMutableArray new];
         fullEventHistory = [NSMutableArray new];
-        eligibleEventHistory = [NSMutableArray new];
         
         dice = [Dice new];
         score = 0;
@@ -48,6 +48,99 @@
     return self;
 }
 
+#pragma MARK public methods
+
+- (Event *) getInitialEvent {
+    
+    Event *initialEvent = [Event new];
+    Choice *moveForward = [[Choice alloc] initWithChoiceDescription: @"Move Forward"];
+    NSMutableArray *initialChoices = [NSMutableArray arrayWithObjects:moveForward, nil];
+    if ([currentEnvironment isKindOfClass: [ForrestEnviroment class]]) {
+        Choice *gatherWood = [[Choice alloc] initWithChoiceDescription: @"Gather Wood"];
+        [initialChoices addObject: gatherWood];
+    } else if ([currentEnvironment isKindOfClass: [Mountain class]]) {
+        if ([dice isRollSuccessfulWithNumberOfDice:1 sides:6 bonus:0 againstTarget:4]) {
+            Choice *gatherMetal = [[Choice alloc] initWithChoiceDescription:@"Gather Metal"];
+            [initialChoices addObject:gatherMetal];
+        }
+    }
+    Event *eventModel = currentEnvironment.events[0];
+    initialEvent.eventDescription = eventModel.eventDescription;
+    initialEvent.choices = initialChoices;
+    
+    currentEventDescription = [initialEvent description];
+    [self manageAdditionToHistories: initialEvent];
+    
+    return initialEvent;
+}
+
+// this method is the heard and soul of this class.
+// it makes use of the selectorsForChoiceDescription dictionary which is
+// initialized in the method 'initializeSelectorsForChoiceDescription.
+// the selectorsForChoiceDescription dictionary matches choice descriptions to methods
+- (Event *) getEventFromChoice:(Choice *)choice {
+    
+    // will be populated (or not!) by some method
+    Event *nextEventModel = nil;
+    
+    // we will eventually return nextEvent
+    Event *nextEvent = nil;
+    
+    if (choice && [choice isKindOfClass:[Choice class]]) {
+        // the choice description will determine what method to use to handle the event
+        NSString *choiceDescription = choice.choiceDescription;
+        
+        // check to see if we have a designated handler for that choice description
+        NSValue *selectorValue = [selectorsForChoiceDescription objectForKey: choiceDescription];
+        
+        if (selectorValue != nil) {
+            // if we do have a designated selector for that choice description
+            // get the selector out of the NSValue wrapper
+            SEL selector = [selectorValue pointerValue];
+            
+            // make sure the selector is real just to be safe
+            if ([self respondsToSelector: selector]) {
+                
+                // and perform that selector!
+                // suppressing warning for potential leak.
+                // i am checking to make sure we respond to the selector.
+                // this would be dangerous if we didn't have 100% control over the selectors available
+                #pragma clang diagnostic push
+                #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+                nextEventModel = [self performSelector: selector];
+                #pragma clang diagnostic pop
+            }
+            
+        } else {
+            // if the event description does not match a selector we recognize, it must be a unique choice
+            nextEventModel = [self handleUniqueChoice: choice];
+        }
+        
+        if (nextEventModel) {
+            // create the nextEvent based upon the one returned by the selector
+            // it may have different choices (illegal choices will be filtered out)
+            nextEvent = [Event new];
+            nextEvent.eventDescription = [nextEventModel eventDescription];
+            nextEvent.choices = [self getLegalChoicesForEvent: nextEventModel];
+        }
+    }
+    
+    // finally, handle history and let the currentEventDescription reflect the nextEvent
+    if (nextEvent) {
+        [self manageAdditionToHistories: nextEvent];
+        currentEventDescription = [nextEvent description];
+    } else {
+        currentEventDescription = nil;
+    }
+    
+    return nextEvent;
+}
+
+- (NSInteger) getScore {
+    return score;
+}
+
+#pragma MARK private methods
 // this dictionary is at the heart of this class.
 // instead of relying on an obscene if/else chain or less
 // than ideal switch statement, we can have O(1) complexity of steps
@@ -85,46 +178,18 @@
     selectorsForChoiceDescription = [NSDictionary dictionaryWithObjects: selectors forKeys: choiceDescriptions];
 }
 
-- (Event *) getInitialEvent {
-    
-    Event *initialEvent = [Event new];
-    Choice *moveForward = [[Choice alloc] initWithChoiceDescription: @"Move Forward"];
-    NSMutableArray *initialChoices = [NSMutableArray arrayWithObjects:moveForward, nil];
-    if ([currentEnvironment isKindOfClass: [ForrestEnviroment class]]) {
-        Choice *gatherWood = [[Choice alloc] initWithChoiceDescription: @"Gather Wood"];
-        [initialChoices addObject: gatherWood];
-    } else if ([currentEnvironment isKindOfClass: [Mountain class]]) {
-        if ([dice isRollSuccessfulWithNumberOfDice:1 sides:6 bonus:0 againstTarget:4]) {
-            Choice *gatherMetal = [[Choice alloc] initWithChoiceDescription:@"Gather Metal"];
-            [initialChoices addObject:gatherMetal];
-        }
-    }
-    Event *eventModel = currentEnvironment.events[0];
-    initialEvent.eventDescription = eventModel.eventDescription;
-    initialEvent.choices = initialChoices;
-    
-    currentEventDescription = [initialEvent description];
-    [self addEventToHistories: initialEvent];
-    
-    return initialEvent;
-}
+// history
 
-- (NSInteger) getScore {
-    return score;
-}
-
-#pragma MARK - For managing history
-
-- (void) addEventToHistories: (Event *)event {
+- (void) manageAdditionToHistories: (Event *)event {
     if (event) {
         [fullEventHistory addObject: event];
-        if ([self eventIsEligibleForHistory: event]) {
-            [eligibleEventHistory addObject: event];
+        if ([self isEligibleForHistory: event]) {
+            [eventHistory addObject: event];
         }
     }
 }
 
-- (BOOL) eventIsEligibleForHistory: (Event *)event {
+- (BOOL) isEligibleForHistory: (Event *)event {
     BOOL eligibleForHistory = YES;
     if (event.isCombatEvent || event.isUnique) {
         eligibleForHistory = NO;
@@ -132,70 +197,7 @@
     return eligibleForHistory;
 }
 
-#pragma MARK - For handling actions
-
-// this method is the heard and soul of this class.
-// it makes use of the selectorsForChoiceDescription dictionary which is
-// initialized in the method 'initializeSelectorsForChoiceDescription.
-// the selectorsForChoiceDescription dictionary matches choice descriptions to methods
-- (Event *) getEventFromChoice:(Choice *)choice {
-    
-    // will be populated (or not!) by some method
-    Event *nextEventModel = nil;
-    
-    // the choice description will determine what method to use to handle the event
-    NSString *choiceDescription = choice.choiceDescription;
-    
-    // check to see if we have a designated handler for that choice description
-    NSValue *selectorValue = [selectorsForChoiceDescription objectForKey: choiceDescription];
-    
-    if (selectorValue != nil) {
-        // if we do have a designated selector for that choice description
-        // get the selector out of the NSValue wrapper
-        SEL selector = [selectorValue pointerValue];
-        
-        // make sure the selector is real just to be safe
-        if ([self respondsToSelector: selector]) {
-            
-            // and perform that selector!
-            // suppressing warning for potential leak.
-            // i am checking to make sure we respond to the selector.
-            // this would be dangerous if we didn't have 100% control over the selectors available
-            #pragma clang diagnostic push
-            #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
-            nextEventModel = [self performSelector: selector];
-            #pragma clang diagnostic pop
-        }
-        
-    } else {
-        
-        // if the event description does not match a selector we recognize, it must be a unique choice
-        nextEventModel = [self handleUniqueChoice: choice];
-    }
-    
-    // we will eventually return nextEvent
-    Event *nextEvent = nil;
-    if (nextEventModel) {
-        
-        // create the nextEvent based upon the one returned by the selector
-        // it may have different choices (illegal choices will be filtered out)
-        nextEvent = [Event new];
-        nextEvent.eventDescription = [nextEventModel eventDescription];
-        nextEvent.choices = [self getLegalChoicesForEvent: nextEventModel];
-        
-        // handle history
-        [self addEventToHistories: nextEvent];
-    }
-    
-    // finally, let the currentEventDescription reflect the nextEvent
-    if (nextEvent) {
-        currentEventDescription = [nextEvent description];
-    } else {
-        currentEventDescription = nil;
-    }
-    
-    return nextEvent;
-}
+// filter choices
 
 - (NSMutableArray *) getLegalChoicesForEvent:(Event *)event {
     
@@ -226,18 +228,7 @@
     return availableChoices;
 }
 
-- (Event *) moveForward {
-    score += 1;
-    Event *nextEvent;
-    NSInteger index;
-    do {
-        index = [dice rollDieWithSides: [currentEnvironment.events count]] - 1;
-        nextEvent = [currentEnvironment.events objectAtIndex: index];
-    } while (![self isEligible: nextEvent]);
-    return nextEvent;
-}
-
-- (BOOL) isEligible: (Event *)event {
+- (BOOL) isEligibleForRandomSelection: (Event *)event {
     BOOL isEligible = YES;
     if ([[event description] isEqualToString: currentEventDescription] || (event.isUnique && event.hasOccurred)) {
         isEligible = NO;
@@ -245,9 +236,24 @@
     return isEligible;
 }
 
+- (Event *) moveForward {
+    score += 1;
+    Event *nextEvent;
+    NSInteger index;
+    do {
+        index = [dice rollDieWithSides: [currentEnvironment.events count]] - 1;
+        nextEvent = [currentEnvironment.events objectAtIndex: index];
+    } while (![self isEligibleForRandomSelection: nextEvent]);
+    return nextEvent;
+}
+
 - (Event *) moveBackwards {
     score -= 1;
-    return nil;
+    if ([eventHistory count] > 1) {
+        [eventHistory removeLastObject];
+    }
+    Event *lastEvent = [eventHistory lastObject];
+    return lastEvent;
 }
 
 - (Event *) fight {
